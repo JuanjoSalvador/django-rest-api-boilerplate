36a16f5ea8b43ed22ccfcd729416983f7a0a5c5b
Juanjo Salvador <juanjosalvador@netc.eu>
2022-01-23 20:20:22 +0100

    Fix mandatory params checking

--- myapp/api/views.py
+++ myapp/api/views.py
@@ -9,23 +9,20 @@ from rest_framework.response import Response
 class BaseAPIView(viewsets.ViewSet):
     def get(self, request):
         if self.mandatory_params:
-            for param in request.query_params:
-                if param in self.mandatory_params:
-                    bad_request = False
-        else:
-            bad_request = False
-        
-        return bad_request
+            for param in self.mandatory_params:
+                return not request.GET.get(param)
 
 # Remove this class or use it as template
-
 class TestViewset(BaseAPIView):
     mandatory_params = (None)
     permission_classes = (permissions.AllowAny, )
 
     def list(self, request):
         if super().get(request):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={'msg': 'Not all mandatory params were found.'}
+            )
 
         return Response(status=status.HTTP_200_OK, 
                         data={'msg': 'It works!'})
