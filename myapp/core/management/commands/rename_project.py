0079ee0eb7d25bf5fa8471b212c7d0c0f6027958
Juanjo Salvador <juanjosalvador@netc.eu>
2022-01-29 16:56:55 +0100

    fix rename command

--- myapp/core/management/commands/rename_project.py
+++ myapp/core/management/commands/rename_project.py
@@ -17,8 +17,11 @@ class Command(BaseCommand):
 
         # logic for renaming the files
 
-        files_to_rename = [f'{current_project_name}/settings.py',
-                           f'{current_project_name}/wsgi.py', 'manage.py']
+        files_to_rename = [
+                    'manage.py',
+                    f'{current_project_name}/settings/base.py',
+                    f'{current_project_name}/wsgi.py'
+                ]
 
         for f in files_to_rename:
             with open(f, 'r') as file:
